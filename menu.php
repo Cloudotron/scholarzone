a72cad8b099dfbe82972bc2c809d1fd9755b5dd3
Cloudotron <info@cloudotron.com>
2019-10-10 14:19:46 +0530

    little menu change

--- menu.php
+++ menu.php
@@ -1,41 +1,4 @@
 <header id="header" class="header">
-   <!-- <div class="header-top bg-theme-colored border-top-theme-colored2-2px sm-text-center">
-      <div class="container">
-        <div class="row">          
-          <div class="col-md-6">
-            <div class="widget">
-              <ul class="styled-icons icon-sm icon-white">
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="widget">
-              <ul class="list-inline text-right flip sm-text-center">
-                <li>
-                  <a class="text-white" href="#">FAQ</a>
-                </li>
-                <li class="text-white">|</li>
-                <li>
-                  <a class="text-white" href="#">Help Desk</a>
-                </li>
-                <li class="text-white">|</li>
-                <li>
-                  <a class="text-white" href="#">Support</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
-
-
-
     <div class="header-nav">
       <div class="header-nav-wrapper navbar-scrolltofixed bg-white">
         <div class="container">
@@ -49,17 +12,13 @@
                         <li><a href="about-us.php">About Us</a></li>
                         <li><a href="status.php">Form Status</a></li>
                         <li><a href="logout.php">Logout</a></li>
-                        
-                        <!--<li><a href="javascript:void(0)">Shortcodes <span class="label label-danger">New</span></a>-->
                 <?php }else{ ?>
-                      <li><a href="index.php">Home</a></li>
-                      <li><a href="login.php">Student Login</a></li>
-                      <li><a href="./admin">Org Login</a></li>
-                      <li><a href="registration.php">Student Signup</a></li>
-                      <li><a href="provider-registration.php">Org Signup</a></li>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="login.php">Student Login</a></li>
+                        <li><a href="./admin">Org Login</a></li>
+                        <li><a href="registration.php">Student Signup</a></li>
+                        <li><a href="provider-registration.php">Org Signup</a></li>
                 <?php } ?>
-              
-                
               </li>
             </ul>
           </nav>
@@ -67,3 +26,10 @@
       </div>
     </div>
   </header>
+
+  <!--
+1. put the list of scholarship
+2. verify by aadhar and pan
+3. keep track of all the applications in student oortal
+4. 
+    -->
